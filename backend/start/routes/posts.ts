0f14d83e3f205de745adab069807904fccd55481
hugo wemer <hugo.silva@treetech.com.br>
2021-09-19 14:25:15 -0300

    feat: create authentication endpoint on backend

---
+++ backend/start/routes/posts.ts
@@ -0,0 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/', 'PostsController.index')
+  Route.post('/', 'PostsController.store').middleware('auth')
+  Route.get('/:slug', 'PostsController.show')
+  Route.put('/:slug', 'PostsController.update').middleware('auth')
+  Route.delete('/:slug', 'PostsController.destroy').middleware('auth')
+}).prefix('/posts')
